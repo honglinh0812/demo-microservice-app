597b17f21dd6d1cce1765af46da16f614c2c153e
honglinh0812 <honglinh0812uet@gmail.com>
2025-06-19 16:19:24 +0700

    feat(repo): Add helm chart and source code to app repo

---
+++ microservices-backend/src/models/product.py
@@ -0,0 +1,23 @@
+from src.models.user import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50))
+    in_stock = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'category': self.category,
+            'in_stock': self.in_stock
+        }
+
